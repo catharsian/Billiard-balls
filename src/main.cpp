8b6b37ed1d05df877a76d57db2044295498a44cd
catharsian <83705482+catharsian@users.noreply.github.com>
2021-10-10 17:51:44 +0500

    completed project

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,19 @@
 
+#include "endEngine.h"
+
+
+
+
+
+
+
+int main()
+{
+	parser::initialize();
+	parser::parse("./conf.cfg");
+	baseEngine::initialize();
+	endEngine game;
+	game.Start();
+
+	return 0;
+}
